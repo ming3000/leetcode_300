ccdf3e9fb5ec0e009ed145316c6e2c6af9e26c46
xushouming <xushouming@bytedance.com>
2020-11-04 21:11:56 +0800

    39

---
+++ 039_combinationSum.go
@@ -0,0 +1,27 @@
+package leetcode_300
+
+import "sort"
+
+func combinationSum(candidates []int, target int) [][]int {
+	ret := make([][]int, 0)
+	sort.Ints(candidates)
+	dfsCombinationSum(candidates, target, 0, nil, &ret)
+	return ret
+}
+
+func dfsCombinationSum(nums []int, target, start int, path []int, ret *[][]int) {
+	if target < 0 {
+		return
+	}
+	if target == 0 {
+		temp := make([]int, len(path))
+		copy(temp, path)
+		*ret = append(*ret, temp)
+		return
+	}
+	for i := start; i < len(nums); i++ {
+		path = append(path, nums[i])
+		dfsCombinationSum(nums, target-nums[i], i, path, ret)
+		path = path[:len(path)-1]
+	}
+}
