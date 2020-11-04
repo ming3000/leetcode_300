0ae843fb3204ca02f045047e93625fff37617102
xushouming <xushouming@bytedance.com>
2020-11-04 17:40:32 +0800

    9

---
+++ 009_isPalindrome.go
@@ -0,0 +1,14 @@
+package leetcode_300
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	raw, reversed := x, 0
+	for raw != 0 {
+		reversed = reversed*10 + raw%10
+		raw = raw / 10
+	} //>
+	return x == reversed
+}
