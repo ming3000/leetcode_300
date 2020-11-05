b6471ace0ca777a18ab1a4ec9faf2004771fe567
xushouming <xushouming@bytedance.com>
2020-11-05 20:05:23 +0800

    126

--- 125_isPalindrome.go
+++ 125_isPalindrome.go
@@ -1,5 +1,34 @@
 package leetcode_300
 
-func isPalindrome125(s string) bool {
+import "strings"
 
+func isPalindrome125(s string) bool {
+	s = strings.TrimSpace(s)
+	s = strings.ToLower(s)
+	left, right := 0, len(s)-1
+	for left < right {
+		if !isValidChar(s[left]) {
+			left++
+			continue
+		} //>>
+		if !isValidChar(s[right]) {
+			right--
+			continue
+		} //>>
+		if s[left] != s[right] {
+			return false
+		} //>>
+		left++
+		right--
+	} //>
+	return true
+}
+
+func isValidChar(ch byte) bool {
+	if (ch >= '0' && ch <= '9') ||
+		(ch >= 'a' && ch <= 'z') ||
+		(ch >= 'A' && ch <= 'Z') {
+		return true
+	}
+	return false
 }
