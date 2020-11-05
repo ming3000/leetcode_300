af69c396fa73263246a0bafb586d961cf6392607
xushouming <xushouming@bytedance.com>
2020-11-05 18:10:20 +0800

    112

---
+++ 112_hasPathSum.go
@@ -0,0 +1,12 @@
+package leetcode_300
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root.Val == sum
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
