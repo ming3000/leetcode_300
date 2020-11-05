349d4078e4e8dd16f75005bd77668e57c625bbc0
xushouming <xushouming@bytedance.com>
2020-11-05 11:29:00 +0800

    86

---
+++ 086_partition.go
@@ -0,0 +1,21 @@
+package leetcode_300
+
+func partition(head *ListNode, x int) *ListNode {
+	bigSentry, smallSentry := &ListNode{Next: head}, &ListNode{Next: head}
+	bigTail, smallTail := bigSentry, smallSentry
+
+	for head != nil {
+		if head.Val < x {
+			smallTail.Next = head
+			smallTail = smallTail.Next
+		} else {
+			bigTail.Next = head
+			bigTail = bigTail.Next
+		} //>>
+		head = head.Next
+	} //>
+
+	bigTail.Next = nil
+	smallTail.Next = bigSentry.Next
+	return smallSentry.Next
+}
