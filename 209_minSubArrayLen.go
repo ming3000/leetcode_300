54776ecbacc1f59e70003be21c1587f6adb59d3f
xushouming <xushouming@bytedance.com>
2020-11-06 10:45:46 +0800

    209

---
+++ 209_minSubArrayLen.go
@@ -0,0 +1,25 @@
+package leetcode_300
+
+import "math"
+
+func minSubArrayLen(s int, nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	left, right := 0, 0
+	sum, ret := 0, math.MaxInt64
+	for right < len(nums) {
+		sum += nums[right]
+		for sum >= s {
+			ret = minInt(ret, right-left+1)
+			sum -= nums[left]
+			left++
+		} //>>
+		right++
+	} //>
+
+	if ret == math.MaxInt64 {
+		return 0
+	}
+	return ret
+}
