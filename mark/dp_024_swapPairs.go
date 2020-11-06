b881d4591f03540c682deaedd88e23141ef3fefc
xushouming <xushouming@bytedance.com>
2020-11-06 17:16:31 +0800

    mark

---
+++ mark/dp_024_swapPairs.go
@@ -0,0 +1,18 @@
+package mark
+
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	sentry := &ListNode{Next: head}
+	pre := sentry
+	for pre != nil && pre.Next != nil && pre.Next.Next != nil {
+		first, second := pre.Next, pre.Next.Next
+		first.Next = second.Next
+		second.Next = first
+		pre.Next = second
+
+		pre = first
+	}
+	return sentry.Next
+}
