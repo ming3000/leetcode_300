15f4d02b11bdfffb6215cc6e555b9f059086e808
xushouming <xushouming@bytedance.com>
2020-11-06 19:14:37 +0800

    mark

---
+++ mark/dfs_046_permute.go
@@ -0,0 +1,27 @@
+package mark
+
+func permute(nums []int) [][]int {
+	ret := make([][]int, 0)
+	usedNums := make(map[int]bool)
+	dfsPermute(nums, usedNums, nil, &ret)
+	return ret
+}
+
+func dfsPermute(nums []int, usedNums map[int]bool, path []int, ret *[][]int) {
+	if len(path) == len(nums) {
+		temp := make([]int, len(path))
+		copy(temp, path)
+		*ret = append(*ret, temp)
+		return
+	}
+	for _, v := range nums {
+		if usedNums[v] {
+			continue
+		} //>>
+		usedNums[v] = true
+		path = append(path, v)
+		dfsPermute(nums, usedNums, path, ret)
+		path = path[:len(path)-1]
+		usedNums[v] = false
+	} //>
+}
