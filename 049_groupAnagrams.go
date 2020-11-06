c8b4d92327a3918e91b4637b81ee348622945868
xushouming <xushouming@bytedance.com>
2020-11-06 19:51:56 +0800

    mark

--- 049_groupAnagrams.go
+++ 049_groupAnagrams.go
@@ -1,5 +1,33 @@
 package leetcode_300
 
-func groupAnagrams(strs []string) [][]string {
+import (
+	"sort"
+	"strings"
+)
 
+func groupAnagrams(strs []string) [][]string {
+	keyMap := make(map[string][]string)
+	for _, str := range strs {
+		key := generateKey(str)
+		if _, ok := keyMap[key]; !ok {
+			keyMap[key] = make([]string, 0)
+		} //>>
+		keyMap[key] = append(keyMap[key], str)
+	} //>
+
+	ret := make([][]string, 0)
+	for key := range keyMap {
+		ret = append(ret, keyMap[key])
+	}
+	return ret
+}
+
+func generateKey(s string) string {
+	s = strings.TrimSpace(s)
+	s = strings.ToLower(s)
+	b := []byte(s)
+	sort.Slice(b, func(i, j int) bool {
+		return b[i] < b[j]
+	})
+	return string(b)
 }
