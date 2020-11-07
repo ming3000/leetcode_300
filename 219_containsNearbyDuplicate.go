46b835e0638c46cbc4f160d578a261faca673f90
xushouming <xushouming@bytedance.com>
2020-11-07 18:01:14 +0800

    mark

---
+++ 219_containsNearbyDuplicate.go
@@ -0,0 +1,15 @@
+package leetcode_300
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	checkMap := make(map[int]bool)
+	for i := 0; i < len(nums); i++ {
+		if checkMap[nums[i]] {
+			return true
+		} //>>
+		checkMap[nums[i]] = true
+		if len(checkMap) > k {
+			delete(checkMap, nums[i-k])
+		}
+	} //>
+	return false
+}
