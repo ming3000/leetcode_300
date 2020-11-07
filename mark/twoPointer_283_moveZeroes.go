57053e94bbab1e346df1297bf799d8c63dd83560
xushouming <xushouming@bytedance.com>
2020-11-07 18:32:33 +0800

    mark

---
+++ mark/twoPointer_283_moveZeroes.go
@@ -0,0 +1,15 @@
+package mark
+
+func moveZeroes(nums []int) {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != 0 {
+			nums[slow] = nums[fast]
+			slow++
+		} //>>
+	} //>
+
+	for i := slow; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
