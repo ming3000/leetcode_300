841bdb72e047ebd3074b8140c93d7bc0beb79340
xushouming <xushouming@bytedance.com>
2020-11-08 14:42:54 +0800

    mark

--- 027_removeElement.go
+++ 027_removeElement.go
@@ -1,12 +1,12 @@
 package leetcode_300
 
 func removeElement(nums []int, val int) int {
-	for i := 0; i < len(nums); {
-		if nums[i] == val {
-			nums = append(nums[:i], nums[i+1:]...)
-		} else {
-			i++
+	var slow int
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
 		} //>>
-	} // >
-	return len(nums)
+	} //>
+	return slow
 }
